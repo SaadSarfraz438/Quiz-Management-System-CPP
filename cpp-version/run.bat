55065ca9ccc4f5d9945717cea5ce93ae96b7fdc5
Saad Sarfraz <saadsarfraz438@gmail.com>
2026-05-10 20:19:54 +0500

    added run.bat to run code

--- cpp-version/run.bat
+++ cpp-version/run.bat
@@ -1,23 +1,27 @@
 @echo off
 title Quiz Management System
 
+cd /d "%~dp0"
+
 echo ==================================
 echo     Quiz Management System
 echo ==================================
 echo.
 
 echo Compiling project...
-g++ main.cpp -o QuizManagementSystem.exe
+
+g++ main.cpp -o main.exe
 
 if %errorlevel% neq 0 (
     echo.
     echo Compilation failed!
+    echo Make sure MinGW/g++ is installed and added to PATH.
     pause
     exit
 )
 
 echo.
-echo Running Quiz Management System...
-QuizManagementSystem.exe
+echo Running program...
+main.exe
 
 pause
